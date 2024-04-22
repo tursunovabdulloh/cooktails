7567a0c29b9ed82779e383dce7b346b8730f2b04
Abdullohtursunov <tursunovabdulloh172@gmail.com>
2024-04-22 14:05:51 +0300

    1

--- about.js
+++ about.js
@@ -1,62 +1,44 @@
-const urlParams = new URLSearchParams(window.location.search);
-const myParam = urlParams.get("div2");
-
-console.log(myParam);
-
-const loader = document.querySelector("#loader");
+const loader = document.querySelector(".loader");
 const detailsBtn = document.querySelector("#detailsBtn");
 const detailsDiv = document.querySelector("#detailsDiv");
+const urlParams = new URLSearchParams(window.location.search);
+const cocktailid = urlParams.get("id");
+console.log(cocktailid);
 
-function creatDetails(data2) {
+fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailid}`)
+  .then((res) => res.json())
+  .then((data) => {
+    loader.classList.add("hidden");
+    creatDetails(data.drinks[0]);
+  });
+
+function creatDetails(data) {
   detailsDiv.innerHTML = "";
-  data2.forEach(
-    ({
-      idDrink,
-      strDrinkThumb,
-      strDrink,
-      strCategory,
-      strAlcoholic,
-      strGlass,
-      strInstructions,
-      strIngredient1,
-      strIngredient2,
-      strIngredient3,
-      strIngredient4,
-    }) => {
-      const div2 = document.createElement("div");
-      div2.classList.add("card2");
-      detailsDiv.innerHTML = `
+  const div = document.createElement("div");
+  div.classList.add("card");
+  detailsDiv.innerHTML = `
       <a id="detailsBtn" class="btn btn-primary" href="./index.html">back home</a>
-        <h2 class="section-title">${strDrink}</h2>
+        <h2 class="section-title">${data.strDrink}</h2>
         <div class="drink">
           <img
-            src="${strDrinkThumb}"
-            alt="${strDrinkThumb}"
+            src="${data.strDrinkThumb}"
+            alt="${data.strDrinkThumb}"
           />
           <div class="drink-info">
-            <p><span class="drink-data">name :</span> ${strDrink}</p>
-            <p><span class="drink-data">category :</span> ${strCategory}</p>
-            <p><span class="drink-data">info :</span> ${strAlcoholic}</p>
-            <p><span class="drink-data">glass :</span> ${strGlass}</p>
+            <p><span class="drink-data">name :</span> ${data.strDrink}</p>
+            <p><span class="drink-data">category :</span> ${data.strCategory}</p>
+            <p><span class="drink-data">info :</span> ${data.strAlcoholic}</p>
+            <p><span class="drink-data">glass :</span> ${data.strGlass}</p>
             <p>
-              <span class="drink-data">instructons :</span> ${strInstructions}
+              <span class="drink-data">instructons :</span> ${data.strInstructions}
             </p>
             <p>
-              <span class="drink-data">ingredients :</span><span> ${strIngredient1}</span
-              ><span> ${strIngredient2}</span><span> ${strIngredient3}</span
-              ><span> ${strIngredient4}</span>
+              <span class="drink-data">ingredients :</span><span> ${data.strIngredient1}</span
+              ><span> ${data.strIngredient2}</span><span> ${data.strIngredient3}</span
+              ><span> ${data.strIngredient4}</span>
             </p>
           </div>
         </div>
-        <p>${idDrink}</p>
       `;
-      detailsDiv.appendChild(div2);
-    }
-  );
+  detailsDiv.appendChild(div);
 }
-fetch(`www.thecocktaildb.com/api/json/v1/1/lookup.php?i=18222`)
-  .then((res) => res.json())
-  .then((data2) => {
-    loader.classList.add("hidden");
-    creatDetails(data2);
-  });
--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const btn = document.querySelector(".btn");
 const coctailCard = document.querySelector("#coctailCard");
 const loader = document.querySelector("#loader");
 const input = document.querySelector("#input");
+
+fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
+  .then((res) => res.json())
+  .then((data) => {
+    loader.classList.add("hidden");
+    creatCoktails(data.drinks);
+  });
+
 function creatCoktails(data) {
   coctailCard.innerHTML = "";
   data.forEach(
@@ -12,23 +20,21 @@ function creatCoktails(data) {
    <article class="cocktail">
    <div class="img-container"><img src="${strDrinkThumb}" alt="A1"></div>
    <div class="cocktail-footer"><h3>${strDrink}</h3><h4>${strGlass}</h4><p>${strAlcoholic}</p>
-   <a id="detailsBtn" class="btn btn-primary btn-details" href="./about.html?div2=${idDrink}">details</a></div></article>
+   <a  class="btn btn-primary btn-details" href="./about.html?id=${idDrink}">details</a></div>
+   </article>
       `;
       coctailCard.appendChild(div);
+      console.log(idDrink);
     }
   );
 }
-fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
-  .then((res) => res.json())
-  .then((data) => {
-    loader.classList.add("hidden");
-    creatCoktails(data.drinks);
-  });
 
 input.addEventListener("input", (e) => {
-  console.log(e.target.value);
-  const url = ` www.thecocktaildb.com/api/json/v1/1/search.php?s=${e.target.value}`;
-  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${url}`)
+  coctailCard.innerHTML = "";
+  loader.classList.remove("hidden");
+  fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${e.target.value}`
+  )
     .then((res) => res.json())
     .then((data) => {
       loader.classList.add("hidden");
