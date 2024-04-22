513425da0ce04d3dce4c8c3c6b82f03031f495f8
Abdullohtursunov <tursunovabdulloh172@gmail.com>
2024-04-22 08:01:48 +0300

    f

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const btn = document.querySelector(".btn");
 const coctailCard = document.querySelector("#coctailCard");
 const loader = document.querySelector("#loader");
-
+const input = document.querySelector("#input");
 function creatCoktails(data) {
   coctailCard.innerHTML = "";
   data.forEach(
@@ -12,7 +12,7 @@ function creatCoktails(data) {
    <article class="cocktail">
    <div class="img-container"><img src="${strDrinkThumb}" alt="A1"></div>
    <div class="cocktail-footer"><h3>${strDrink}</h3><h4>${strGlass}</h4><p>${strAlcoholic}</p>
-   <a id="detailsBtn" class="btn btn-primary btn-details" href="./about. html?div2=${idDrink}">details</a></div></article>
+   <a id="detailsBtn" class="btn btn-primary btn-details" href="./about.html?div2=${idDrink}">details</a></div></article>
       `;
       coctailCard.appendChild(div);
     }
@@ -24,3 +24,14 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
     loader.classList.add("hidden");
     creatCoktails(data.drinks);
   });
+
+input.addEventListener("input", (e) => {
+  console.log(e.target.value);
+  const url = ` www.thecocktaildb.com/api/json/v1/1/search.php?s=${e.target.value}`;
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${url}`)
+    .then((res) => res.json())
+    .then((data) => {
+      loader.classList.add("hidden");
+      creatCoktails(data.drinks);
+    });
+});
